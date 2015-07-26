f72a314fb8e17dad23eca6cfa8cb1d0c386de1bc
Anna Shipman <anna@annashipman.co.uk>
2015-07-26 22:23:41 +0100

    Code to make the unit tests pass
    This reads in the content of the CSV and stores each row as a hashref where the
    identifier is the `track_upc`. Potentially the id makes more sense, but the
    track_upc is also unique and allows for selecting by that rather than iterating
    through all rows each time.
    
    The unit tests all pass, though I do not handle the empty string properly so
    there is a warning.

--- lib/CsvPerl.pm
+++ lib/CsvPerl.pm
@@ -12,15 +12,36 @@ sub new {
     my $self = {};
     bless $self, $class;
 
+    my $fh = $self->{'csv_handle'};
+
     $self->{'csv'} = Text::CSV->new ( { binary => 1 } );
-    open $self->{'csv_handle'}, '<:encoding(utf8)', $csv_file
+    open $fh, '<:encoding(utf8)', $csv_file
         or return undef;
 
+    $self->{'csv'}->column_names ( $self->{'csv'}->getline ( $fh ) );
+
+    while ( my $row = $self->{'csv'}->getline_hr ( $fh ) ) {
+        my $track_upc = $row->{'track_upc'};
+        $self->{"rows{$track_upc}"} = $row;
+    }
+
+    close $fh;
     return $self;
 }
 
 sub sum_tracks_by_code {
-    return 0;
+    my $self = shift;
+    my @track_upcs = @_;
+    my $track_cost = 0;
+
+    foreach my $track ( @track_upcs ) {
+        my $csv_track = $self->{"rows{$track}"};
+        if ( $track eq $csv_track->{'track_upc'} ) {
+            $track_cost = $track_cost + $csv_track->{'track_price'};
+        }
+    }
+
+    return $track_cost;
 }
 
 1;
